dc1381170c0e2a258b30d49caed980ad275ae0b5
tsabrandon <tsabrandon@gmail.com>
2019-10-30 02:16:31 +0800

    Updates

---
+++ precache-manifest.2a1208a55c63228418f5357a5697da77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "55424af91dde45a42413",
+    "url": "/phonestore/static/css/main.4f74e215.chunk.css"
+  },
+  {
+    "revision": "55424af91dde45a42413",
+    "url": "/phonestore/static/js/main.efb276c4.chunk.js"
+  },
+  {
+    "revision": "511939dba0c1763c0043",
+    "url": "/phonestore/static/js/runtime~main.d1a3666d.js"
+  },
+  {
+    "revision": "06ed4092c0140725f5ae",
+    "url": "/phonestore/static/css/2.2dcec150.chunk.css"
+  },
+  {
+    "revision": "06ed4092c0140725f5ae",
+    "url": "/phonestore/static/js/2.3c3b1735.chunk.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/phonestore/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "98bd40270582ed3d1b2868c5718ca68e",
+    "url": "/phonestore/index.html"
+  }
+];
