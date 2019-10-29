f9f7b2d81abae4e71dcad8d39423f5a3afe6426c
tsabrandon <tsabrandon@gmail.com>
2019-10-30 04:34:33 +0800

    changes dollar to peso

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -21,19 +21,19 @@ export default function CartTotals({value, history}) {
                         <span className="text-title">
                             subtotal: 
                         </span>
-                        <strong>${cartSubTotal}</strong>
+                        <strong>₱{cartSubTotal}</strong>
                     </h5>
                     <h5>
                         <span className="text-title">
                             tax: 
                         </span>
-                        <strong>${cartTax}</strong>
+                        <strong>₱{cartTax}</strong>
                     </h5>
                     <h5>
                         <span className="text-title">
                             total: 
                         </span>
-                        <strong>${cartTotal}</strong>
+                        <strong>₱{cartTotal}</strong>
                     </h5>
                     {/* <PayPalButton total={cartTotal} clearCart={clearCart} history={history}/> */}
                 </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -44,7 +44,7 @@ export default class Product extends Component {
                 {title}
               </p>
               <h5 className="text-blue font-italic mb-0">
-                <span className="mr-1">$</span>
+                <span className="mr-1">₱</span>
                 {price}
               </h5>
           </div>
